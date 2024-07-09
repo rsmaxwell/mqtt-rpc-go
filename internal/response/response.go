6ec65fa95ee82d75c12b4324b229fdc756b5bfcd
Richard Maxwell <richard@rsmaxwell.co.uk>
2024-07-09 18:33:08 +0100

    add quit flag to handlers

--- internal/response/response.go
+++ internal/response/response.go
@@ -6,31 +6,28 @@ import (
 	"net/http"
 )
 
-type Response struct {
-	Data map[string]interface{} `json:"data"`
-}
+type Response map[string]interface{}
 
 func New() *Response {
-	var r Response
-	r.Data = make(map[string]interface{})
+	r := make(Response)
 	return &r
 }
 
 func Success() *Response {
 	r := New()
-	r.Data["code"] = http.StatusOK
+	(*r)["code"] = http.StatusOK
 	return r
 }
 
 func BadRequest() *Response {
 	r := New()
-	r.Data["code"] = http.StatusBadRequest
+	(*r)["code"] = http.StatusBadRequest
 	return r
 }
 
 func BadInternalServerError() *Response {
 	r := New()
-	r.Data["code"] = http.StatusInternalServerError
+	(*r)["code"] = http.StatusInternalServerError
 	return r
 }
 
@@ -43,7 +40,7 @@ func (r Response) Ok() bool {
 }
 
 func (r Response) PutCode(code int) {
-	r.Data["code"] = code
+	r["code"] = code
 }
 
 func (r Response) GetCode() (int, error) {
@@ -55,7 +52,7 @@ func (r Response) GetCode() (int, error) {
 }
 
 func (r Response) PutMessage(message string) {
-	r.Data["message"] = message
+	r["message"] = message
 }
 
 func (r Response) GetMessage() (string, error) {
@@ -63,11 +60,11 @@ func (r Response) GetMessage() (string, error) {
 }
 
 func (r Response) PutString(key string, value string) {
-	r.Data[key] = value
+	r[key] = value
 }
 
 func (r Response) GetString(key string) (string, error) {
-	value := r.Data[key]
+	value := r[key]
 	v, ok := value.(string)
 	if !ok {
 		return "", fmt.Errorf("unexpected type for '%s': %+v", key, value)
@@ -76,7 +73,7 @@ func (r Response) GetString(key string) (string, error) {
 }
 
 func (r Response) PutInteger(key string, value int64) {
-	r.Data[key] = value
+	r[key] = value
 }
 
 func (r Response) GetInteger(key string) (int64, error) {
@@ -85,11 +82,11 @@ func (r Response) GetInteger(key string) (int64, error) {
 }
 
 func (r Response) PutNumber(key string, value float64) {
-	r.Data[key] = value
+	r[key] = value
 }
 
 func (r Response) GetNumber(key string) (float64, error) {
-	value := r.Data[key]
+	value := r[key]
 	v, ok := value.(float64)
 	if !ok {
 		return 0, fmt.Errorf("unexpected type for '%s': %+v", key, value)
@@ -98,11 +95,11 @@ func (r Response) GetNumber(key string) (float64, error) {
 }
 
 func (r Response) PutBoolean(key string, value bool) {
-	r.Data[key] = value
+	r[key] = value
 }
 
 func (r Response) GetBoolean(key string) (bool, error) {
-	value := r.Data[key]
+	value := r[key]
 	v, ok := value.(bool)
 	if !ok {
 		return false, fmt.Errorf("unexpected type for '%s': %+v", key, value)
