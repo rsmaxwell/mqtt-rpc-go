65f05bdacb985e593e64ccb3cc3e2f49ba6b1bc9
Richard Maxwell <richard@rsmaxwell.co.uk>
2024-07-10 10:24:16 +0100

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,37 @@
 #!/bin/sh
 
-set -x
 
-BUILD_DIR=$(pwd)/build
-DIST_DIR=$(pwd)/dist
-INFO_DIR=${BUILD_DIR}/info
+BASEDIR=$(dirname "$0")
+SCRIPT_DIR=$(cd $BASEDIR && pwd)
+PROJECT_DIR=$(dirname $SCRIPT_DIR)
+BUILD_DIR=${PROJECT_DIR}/build
+DIST_DIR=${PROJECT_DIR}/dist
 
-. ${INFO_DIR}/maven.sh
+. ${BUILD_DIR}/buildinfo
 
-ZIPFILE=${DIST_DIR}/${NAME}.zip
 
-mvn --batch-mode deploy:deploy-file \
+
+if [ -f ${HOME}/.m2/maven-repository-info ]; then
+    . ${HOME}/.m2/maven-repository-info
+elif [ -f ./maven-repository-info ]; then
+    . ./maven-repository-info
+fi
+
+if [ -z "${MAVEN_REPOSITORY_BASE_URL}" ]; then
+    echo "'MAVEN_REPOSITORY_BASE_URL' is not defined"
+    exit 1
+fi
+
+REPOSITORY_URL="${MAVEN_REPOSITORY_BASE_URL}/${REPOSITORY}"
+
+
+
+mvn --batch-mode --errors deploy:deploy-file \
 	-DgroupId=${GROUPID} \
 	-DartifactId=${ARTIFACTID} \
 	-Dversion=${VERSION} \
 	-Dpackaging=${PACKAGING} \
-	-Dfile=${ZIPFILE} \
+	-Dfile=${DIST_DIR}/${ZIPFILE} \
 	-DrepositoryId=${REPOSITORYID} \
-	-Durl=${URL}
+	-Durl=${REPOSITORY_URL}
+
