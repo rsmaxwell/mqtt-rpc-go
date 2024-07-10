89e9df6b57d271a0dacc65ea4679046b75a277b2
Richard Maxwell <richard@rsmaxwell.co.uk>
2024-07-10 10:45:47 +0100

    update package script

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-set -x
+BASEDIR=$(dirname "$0")
+SCRIPT_DIR=$(cd $BASEDIR && pwd)
+PROJECT_DIR=$(dirname $SCRIPT_DIR)
+BUILD_DIR=${PROJECT_DIR}/build
+PACKAGE_DIR=${PROJECT_DIR}/package
+DIST_DIR=${PROJECT_DIR}/dist
 
-BUILD_DIR="$(pwd)/build"
-PACKAGE_DIR="$(pwd)/package"
-DIST_DIR="$(pwd)/dist"
-INFO_DIR=${BUILD_DIR}/info
-
-. ${INFO_DIR}/maven.sh
-
-ZIPFILE=${NAME}.zip
+. ${BUILD_DIR}/buildinfo
 
 rm -rf ${PACKAGE_DIR} ${DIST_DIR}
 mkdir -p ${PACKAGE_DIR} ${DIST_DIR}
 
 cd ${PACKAGE_DIR}
-cp -r ${BUILD_DIR}/* .
+cp ${BUILD_DIR}/Responder .
+cp ${BUILD_DIR}/buildinfo .
 
-zip -r ${DIST_DIR}/${ZIPFILE} *
+zip ${DIST_DIR}/${ZIPFILE} *
